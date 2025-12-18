874b198c85c04aaff2b2230f2df34448d63dd007
Wilfried-Tech <wilfriedtech.dev@gmail.com>
2025-12-18 18:53:03 +0100

    Ajout de la correction pour le travail 004-intro-php

---
+++ 004-intro-php/correction/index.php
@@ -0,0 +1,100 @@
+<?php
+$user = [
+        "nom" => "John",
+        "prenom" => "Doe",
+        "age" => 20
+];
+
+$isConnected = false;
+
+$navigation = [
+        [
+                'url' => '#',
+                'label' => 'Accueil',
+        ],
+        [
+                'url' => '#',
+                'label' => 'Mes Projets',
+        ],
+        [
+                'url' => '#',
+                'label' => 'Contact',
+        ],
+        [
+                'url' => '#',
+                'label' => 'Mes Services',
+        ],
+        [
+                'url' => '#',
+                'label' => 'Mon CV',
+        ],
+        [
+                'url' => '#',
+                'label' => 'Se Connecter',
+                'enable'    => $isConnected === false
+        ],
+        [
+                'url' => '#',
+                'label' => 'S\'inscrire',
+            'enable'    => $isConnected === false
+        ],
+        [
+                'url' => '#',
+                'label' => 'Mon Compte',
+            'enable'    => $isConnected === true
+        ]
+];
+?>
+
+<!Doctype>
+<html>
+<head>
+    <title>Intro PHP</title>
+    <style>
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    </style>
+</head>
+<body>
+<nav>
+    <ul>
+
+        <?php
+
+            foreach ($navigation as $item) {
+                $url = $item['url'];
+                $label = $item['label'];
+//                $enable = $item['enable'];
+
+                // Si enable a ete defini dans les proprietes de l'obj
+                // alors on lui attribu cette valeur, dans la cas
+                // contraire, on lui donne la valeur true par defaut
+//                if(isset($item['enable'])) {
+//                    $enable = $item['enable'];
+//                } else {
+//                    $enable = true;
+//                }
+
+                // refactoring avec les conditions ternaires
+                $enable = isset($item['enable']) ? $item['enable'] : true;
+
+                if($enable) {
+                    echo '<li><a href="' . $url . '">' . $label . '</a></li>';
+                }
+            }
+        ?>
+
+    </ul>
+</nav>
+</body>
+</html>
